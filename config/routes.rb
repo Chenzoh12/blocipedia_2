9f1914245fd042a42872924172772947ee858df5
Vincent Scotti <ScottiVincentJ@gmail.com>
2017-11-29 07:57:39 -0500

    Problems with rspec since switching to c9.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
     get 'welcome/index'
-
     get 'welcome/about'
+    get 'wikis/index'
 
     root 'welcome#index'
 end
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    let(:name) { RandomData.random_sentence }
+    let(:description) { RandomData.random_paragraph }
+    let(:title) { RandomData.random_sentence }
+    let(:body) { RandomData.random_paragraph }
+
+    it { is_expected.to have_many(:comments) }
+
+    # Need to fine tune this facotry
+    #it { is_expected.to belong_to(:user) }
+
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:body) }
+    #it { is_expected.to validate_presence_of(:user) }
+
+    describe "attributes" do
+        it "has a title and body attributes" do
+            expect(post).to have_attributes(title: post.title, body: post.body)
+        end
+    end
+
+
 end
